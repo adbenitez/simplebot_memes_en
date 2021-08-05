b7dc27d9dbbda418837e2b7228582481844aa54c
adbenitez <asieldbenitez@gmail.com>
2021-08-04 21:45:01 -0400

    fix linter warnings

--- simplebot_memes_en.py
+++ simplebot_memes_en.py
@@ -1,6 +1,9 @@
+"""Plugin's hooks and commands definitions."""
+
 import io
 import mimetypes
 import re
+from typing import Callable
 
 import bs4
 import requests
@@ -32,47 +35,47 @@ def memecenter(bot: DeltaBot, replies: Replies) -> None:
 
 def _get_image() -> tuple:
     url = "https://www.memecenter.com"
-    with requests.get(url, headers=HEADERS) as r:
-        r.raise_for_status()
-        soup = bs4.BeautifulSoup(r.text, "html.parser")
+    with requests.get(url, headers=HEADERS) as resp:
+        resp.raise_for_status()
+        soup = bs4.BeautifulSoup(resp.text, "html.parser")
     url = soup.find("a", class_="random")["href"]
-    with requests.get(url, headers=HEADERS) as r:
-        r.raise_for_status()
-        soup = bs4.BeautifulSoup(r.text, "html.parser")
+    with requests.get(url, headers=HEADERS) as resp:
+        resp.raise_for_status()
+        soup = bs4.BeautifulSoup(resp.text, "html.parser")
     img_desc = soup.title.get_text().strip()
     img_url = soup.find("div", id="fdc_download").a["href"]
     return (img_desc, img_url)
 
 
-def _get_meme(bot, get_image) -> dict:
+def _get_meme(bot: DeltaBot, get_image: Callable) -> dict:
     img = b""
     max_meme_size = int(_getdefault(bot, "max_meme_size"))
     for _ in range(10):
         img_desc, img_url = get_image()
-        with requests.get(img_url, headers=HEADERS) as r:
-            r.raise_for_status()
-            if len(r.content) <= max_meme_size:
-                img = r.content
-                ext = _get_ext(r) or ".jpg"
+        with requests.get(img_url, headers=HEADERS) as resp:
+            resp.raise_for_status()
+            if len(resp.content) <= max_meme_size:
+                img = resp.content
+                ext = _get_ext(resp) or ".jpg"
                 break
-            if not img or len(img) > len(r.content):
-                img = r.content
-                ext = _get_ext(r) or ".jpg"
+            if not img or len(img) > len(resp.content):
+                img = resp.content
+                ext = _get_ext(resp) or ".jpg"
 
     text = "{}\n\n{}".format(img_desc, img_url)
     return dict(text=text, filename="meme" + ext, bytefile=io.BytesIO(img))
 
 
-def _get_ext(r) -> str:
-    d = r.headers.get("content-disposition")
-    if d is not None and re.findall("filename=(.+)", d):
-        fname = re.findall("filename=(.+)", d)[0].strip('"')
+def _get_ext(resp: requests.Response) -> str:
+    disp = resp.headers.get("content-disposition")
+    if disp is not None and re.findall("filename=(.+)", disp):
+        fname = re.findall("filename=(.+)", disp)[0].strip('"')
     else:
-        fname = r.url.split("/")[-1].split("?")[0].split("#")[0]
+        fname = resp.url.split("/")[-1].split("?")[0].split("#")[0]
     if "." in fname:
         ext = "." + fname.rsplit(".", maxsplit=1)[-1]
     else:
-        ctype = r.headers.get("content-type", "").split(";")[0].strip().lower()
+        ctype = resp.headers.get("content-type", "").split(";")[0].strip().lower()
         if ctype == "text/plain":
             ext = ".txt"
         elif ctype == "image/jpeg":
@@ -91,6 +94,8 @@ def _getdefault(bot: DeltaBot, key: str, value=None) -> str:
 
 
 class TestPlugin:
+    """Online tests"""
+
     def test_memecenter(self, mocker):
         msg = mocker.get_one_reply("/memecenter")
         assert msg.is_image()
